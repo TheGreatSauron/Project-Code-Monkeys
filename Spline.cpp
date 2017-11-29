e581e02762b2867900bdc04cd2f097e208f1d5eb
The Great Sauron <33473797+TheGreatSauron@users.noreply.github.com>
2017-11-29 14:49:16 -0700

    Added first implementtation
    Can now handle movement without curves

--- Spline.cpp
+++ Spline.cpp
@@ -1,13 +1,49 @@
 #include "Spline.h"
+#include <cmath>
 
-Spline::Spline() : curve(0) {}
+Spline::Spline() : currentNode(0) {}
 
 void Spline::offset(sf::Vector2f offset)
 {
-    std::queue<sf::Vector2f> tempQueue;
-
-    for (sf::Vector2f currentVector : positionQueue)
+    for (Node& currentNode : nodeVector)
     {
-
+        currentNode.position += offset;
     }
+}
+
+void Spline::addNode(sf::Vector2f position, float curve)
+{
+    Node newNode;
+    newNode.position = position;
+    newNode.curve = curve;
+
+    nodeVector.push_back(newNode);
+}
+
+sf::Vector2f Spline::getDirection(sf::Vector2f position)
+{
+    sf::Vector2f direction;
+    if (nodeVector[currentNode].curve == 0)
+    {
+        direction.x = position.x - nodeVector[currentNode].position.x;
+        direction.y = position.y - nodeVector[currentNode].position.y;
+
+        float magnitude = std::sqrt(std::pow(direction.x, 2) + std::pow(direction.y, 2));
+        direction.x /= magnitude;
+        direction.y /= magnitude;
+    }
+    else
+    {
+        float nodeDistance;
+        sf::Vector2f ;
+    }
+
+    return direction;
+}
+
+bool Spline::iterate()
+{
+    currentNode++;
+
+    return !currentNode >= nodeVector.size();
 }
--- Spline.h
+++ Spline.h
@@ -1,18 +1,25 @@
 #ifndef SPLINE_H_INCLUDED
 #define SPLINE_H_INCLUDED
 
-#include <queue>
+#include <vector>
 #include <SFML/System.hpp>
 
 class Spline
 {
-    std::queue<sf::Vector2f> positionQueue;
-    float curve;
+    struct Node
+    {
+        sf::Vector2f position;
+        float curve;
+    };
+    std::vector<Node> nodeVector;
+    unsigned currentNode;
 
 public:
     Spline();
     void offset(sf::Vector2f offset);
+    void addNode(sf::Vector2f position, float curve = 0);
     sf::Vector2f getDirection(sf::Vector2f position);
+    bool iterate();
 };
 
 #endif // SPLINE_H_INCLUDED
