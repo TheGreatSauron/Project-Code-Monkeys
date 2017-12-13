2f8884e8a578fd12f5db5380712001a37f04d028
The Great Sauron <33473797+TheGreatSauron@users.noreply.github.com>
2017-12-13 14:40:54 -0700

    Changed file layout
    Resources not working properly in the IDE, now fixed

--- Resources.h
+++ Resources.h
@@ -12,13 +12,13 @@ public:
 	//loads all textures when called, if one fails it returns false, if not then true
 	bool load() {
 		//load arial
-		if (!Arial.loadFromFile("resources/font/arial.ttf")) {
+		if (!Arial.loadFromFile("resource/font/arial.ttf")) {
 			std::cout << "arial";
 			return false;
 		}
 
 		//load Texture
-		if (!errorTexture.loadFromFile("resources/photos/Error.png")) {
+		if (!errorTexture.loadFromFile("resource/photos/Error.png")) {
 			std::cout << "errorTxt";
 			return false;
 		}
