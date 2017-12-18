8255a3ff99fa3a93695a17ef915b9f777100531a
Jon Mulyk <JonathonM3@learn.cssd.ab.ca>
2017-12-18 14:47:42 -0700

    'Music'

---
+++ MusicStream.h
@@ -0,0 +1,14 @@
+#ifndef MUSICSTREAM_H_INCLUDED
+#define MUSICSTREAM_H_INCLUDED
+#include <SoundSteam.hpp>
+
+class MusicStream : public sf::SoundStream
+{
+private:
+
+public:
+
+};
+
+
+#endif // MUSICSTREAM_H_INCLUDED
